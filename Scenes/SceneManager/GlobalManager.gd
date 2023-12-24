f14caf1bcf61cbff0b308db8bd810e9147a268fa
IgnOry <37537092+IgnOry@users.noreply.github.com>
2023-12-24 19:16:11 +0100

    Input (teclado y mando) y SceneManager

---
+++ Scenes/SceneManager/GlobalManager.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+const sceneToload = preload("res://Scenes/FirstBattle.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#Gameplay Input sample
+	if Input.is_action_just_pressed("input_left"):
+		print("left")
+		#send signal?
+		pass
+	elif Input.is_action_just_pressed("input_up"):
+		print("up")
+		#send signal?
+		pass
+	elif Input.is_action_just_pressed("input_down"):
+		print("down")
+		#send signal?
+		pass
+	elif Input.is_action_just_pressed("input_right"):
+		print("right")
+		#send signal?
+		pass
+		
+	#Transition
+	if Input.is_action_just_pressed("ui_accept"):
+		$TransitionScreen.transition()
+	pass
+
+func _on_transition_screen_screen_transitioned():
+	$CurrentScene.get_child(0).queue_free()
+	$CurrentScene.add_child(sceneToload.instantiate())
+	pass # Replace with function body.
+
